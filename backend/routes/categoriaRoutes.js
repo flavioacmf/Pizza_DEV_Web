eac15930ece7ed4fb3eec7423c335bbcab46ab1c
flavioacmf <flavioamorimch@gmail.com>
2024-12-07 11:37:50 -0300

    atualizei as rotas para não solicitar o token

--- backend/routes/categoriaRoutes.js
+++ backend/routes/categoriaRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { verificarAutenticacao } = require('../middlewares/authMiddleware');
+//const { verificarAutenticacao } = require('../middlewares/authMiddleware');
 const { 
     listarCategorias, 
     criarCategoria, 
@@ -9,15 +9,15 @@ const {
 } = require('../controllers/categoriaController');
 
 // Rota para listar categorias com o primeiro produto ativo
-router.get('/', verificarAutenticacao, listarCategorias);
+router.get('/', /*verificarAutenticacao,*/ listarCategorias);
 
 // Rota para criar uma nova categoria
-router.post('/', verificarAutenticacao, criarCategoria);
+router.post('/', /*verificarAutenticacao,*/ criarCategoria);
 
 // Rota para atualizar uma categoria existente
-router.put('/:id', verificarAutenticacao, atualizarCategoria);
+router.put('/:id', /*verificarAutenticacao,*/ atualizarCategoria);
 
 // Rota para excluir uma categoria
-router.delete('/:id', verificarAutenticacao, excluirCategoria);
+router.delete('/:id', /*verificarAutenticacao,*/ excluirCategoria);
 
 module.exports = router;
--- backend/routes/pedidoRoutes.js
+++ backend/routes/pedidoRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { verificarAutenticacao } = require('../middlewares/authMiddleware');
+//const { verificarAutenticacao } = require('../middlewares/authMiddleware');
 const { listarPedidos, criarPedido, atualizarPedido, excluirPedido } = require('../controllers/pedidoController');
 
 // Rota para listar pedidos
-router.get('/', verificarAutenticacao, listarPedidos);
+router.get('/', /*verificarAutenticacao,*/ listarPedidos);
 
 // Rota para criar um novo pedido
-router.post('/', verificarAutenticacao, criarPedido);
+router.post('/', /*verificarAutenticacao,*/ criarPedido);
 
 // Rota para atualizar um pedido existente
-router.put('/:id', verificarAutenticacao, atualizarPedido);
+router.put('/:id', /*verificarAutenticacao,*/ atualizarPedido);
 
 // Rota para excluir um pedido
-router.delete('/:id', verificarAutenticacao, excluirPedido);
+router.delete('/:id', /*verificarAutenticacao,*/ excluirPedido);
 
 module.exports = router;
